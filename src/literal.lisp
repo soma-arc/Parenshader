d605dc65272b6bdb3e632c97eecadb2987db27fe
soma_arc <somaarcr@gmail.com>
2016-06-05 16:06:39 +0900

    Add chain

---
+++ src/literal.lisp
@@ -0,0 +1,20 @@
+(in-package :parenshader)
+(in-readtable :fare-quasiquote)
+
+(defanalyzer @
+  (`(,op ,obj ,@body)
+    (declare (ignorable op))
+    (classify :expr :chain (list (analyze obj)) (mapcar #'analyze body))))
+
+(defun get-chain-obj (node)
+  (first (get-data node)))
+(defun get-chain-args (node)c
+  (get-rest node))
+
+(deftranslator node :chain
+  (let* ((obj (translate (get-chain-obj node)))
+	 (args (mapcar #'translate (get-chain-args node)))
+	 (args (string-join args ".")))
+    (format nil "(~a).~a" obj args)))
+
+(in-readtable :standard)
